ead2dacfab0ddb62331f24c564c3ea29bb3791e6
Aliu691 <aliuom1996@gmail.com>
2026-02-26 22:35:29 +0100

    refactor

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Query, UseGuards, Req } from '@nestjs/common';
+import { ApiResponse } from 'src/utils/api-response';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UsersService } from './users.service';
 
@@ -6,6 +7,18 @@ import { UsersService } from './users.service';
 @UseGuards(JwtAuthGuard)
 export class UserController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getProfile(@Req() req: any) {
+    const user = req.user;
+
+    return ApiResponse.success('User profile retrieved', {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.email === process.env.ADMIN_EMAIL ? 'admin' : 'user',
+    });
+  }
 
   @Get('dashboard')
   getDashboard(
